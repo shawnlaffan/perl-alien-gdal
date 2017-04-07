f784dc9c9d4cc0a15a1e4d27fa32cd1545f1b02b
Jason Mumbulla <jasonmumbulla@gmail.com>
2017-04-07 17:32:26 +1000

    Clean directory of old guf.

--- Alien-libpng/lib/Alien/gdal.pm
+++
@@ -1,9 +0,0 @@
-package Alien::gdal;
-
-use strict;
-use warnings;
-use base qw( Alien::Base );
-
-our $VERSION = '1.00';
-
-1;
--- Alien-xz/lib/Alien/xz.pm
+++
@@ -1,9 +0,0 @@
-package Alien::xz;
-
-use strict;
-use warnings;
-use base qw( Alien::Base );
-
-our $VERSION = '1.00';
-
-1;
