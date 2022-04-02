fdade790405f2feb5cd5ce8d40dddf229ceebd0f
shawnlaffan <shawnlaffan@gmail.com>
2022-04-02 15:46:21 +1100

    bump version to 1.29, update Changes

--- lib/Alien/gdal.pm
+++ lib/Alien/gdal.pm
@@ -11,7 +11,7 @@ use Path::Tiny qw /path/;
 use List::Util qw /uniq/;
 use Alien::proj;
 
-our $VERSION = '1.28';
+our $VERSION = '1.29';
 
 my ($have_geos, $have_proj, $have_spatialite);
 my @have_aliens;
