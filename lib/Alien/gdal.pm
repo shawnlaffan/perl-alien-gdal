2a2406fa55eed131cd241066a72e4a0a096c4c33
shawnlaffan <shawnlaffan@gmail.com>
2022-09-15 09:46:33 +1000

    release 1.33_001

--- lib/Alien/gdal.pm
+++ lib/Alien/gdal.pm
@@ -11,7 +11,7 @@ use Path::Tiny qw /path/;
 use List::Util qw /uniq/;
 use Alien::proj;
 
-our $VERSION = '1.32';
+our $VERSION = '1.33_001';
 
 my ($have_geos, $have_proj, $have_spatialite);
 my @have_aliens;
